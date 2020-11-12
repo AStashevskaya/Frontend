56557070952cd09fda01d16f01c499c370ebea26
AStashevskaya <stashevskayaaa@gmail.com>
2020-11-12 14:18:48 +0300

    fix: trying to make a deploy

--- gem-puzzle/src/js/layouts/images.js
+++
@@ -1,5 +0,0 @@
-// const images = ['clode.jpg', 'dega.jpg', 'eduard.jpg', 'mashkov.jpg', 'renuar.jpg', 'repin.jpg', 'rerih.jpg', 'shagal.jpg']
-let images = [...Array(148).keys()]
-images = images.map(el => `${el+1}.jpg`)
-console.log(images)
-export {images} 
