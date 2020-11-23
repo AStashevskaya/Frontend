2e01c36673a50d3ccc046579333ae66db5fcaffc
AStashevskaya <stashevskayaaa@gmail.com>
2020-11-24 02:15:45 +0300

    feat: add assets

---
+++ english-for-kids/src/js/data/clothes.js
@@ -0,0 +1,50 @@
+export default clothes = [
+  {
+    russian: 'блузка',
+    english: 'blouse',
+    image: 'blouse.jpg',
+    sound: 'blouse.mp3',
+  },
+  {
+    russian: 'платье',
+    english: 'dress',
+    image: 'dress.jpg',
+    sound: 'dress.mp3',
+  },
+  {
+    russian: 'ботинок',
+    english: 'boot',
+    image: 'boot.jpg',
+    sound: 'boot.mp3',
+  },
+  {
+    russian: 'юбка',
+    english: 'shirt',
+    image: 'shirt.jpg',
+    sound: 'shirt.mp3',
+  },
+  {
+    russian: 'юбка',
+    english: 'skirt',
+    image: 'skirt.jpg',
+    sound: 'skirt.mp3',
+  },
+  {
+    russian: 'брюки',
+    english: 'pants',
+    image: 'pants.jpg',
+    sound: 'pants.mp3',
+  },
+  {
+    russian: 'туфля',
+    english: 'shoe',
+    image: 'shoe.jpg',
+    sound: 'shoe.mp3',
+  },
+  {
+    russian: 'пальто',
+    english: 'coat',
+    image: 'coat.jpg',
+    sound: 'coat.mp3',
+  },
+]
